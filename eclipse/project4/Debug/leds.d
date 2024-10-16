5f57a1eac903a63c74ba5a9f82c8ac6561ff3808
FatmaAshraf76 <143643641+FatmaAshraf76@users.noreply.github.com>
2024-10-16 14:23:19 +0300

    Add files via upload

---
+++ eclipse/project4/Debug/leds.d
@@ -0,0 +1,12 @@
+leds.o leds.o: ../leds.c ../common_macros.h ../leds.h ../std_types.h \
+  ../gpio.h ../lcd.h
+
+../common_macros.h:
+
+../leds.h:
+
+../std_types.h:
+
+../gpio.h:
+
+../lcd.h:
---
+++ eclipse/project4/Debug/main.d
@@ -0,0 +1,16 @@
+main.o main.o: ../main.c ../lcd.h ../std_types.h ../icu.h ../gpio.h \
+  ../buzzer.h ../leds.h ../ultrasonic.h
+
+../lcd.h:
+
+../std_types.h:
+
+../icu.h:
+
+../gpio.h:
+
+../buzzer.h:
+
+../leds.h:
+
+../ultrasonic.h:
